b2be6ef5007896486eec35d581d5e30169bfeabb
Aron Griffis <aron@scampersand.com>
2023-11-04 10:04:44 -0400

    feat: all published posts except advent

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,9 +7,11 @@ const date = z
 const posts = defineCollection({
   schema: z.object({
     author: z.string().default('Aron'),
+    created: date.optional(),
     draft: z.boolean().default(false),
     excerpt: z.string().optional(),
     image: z.string().optional(),
+    index: z.boolean().default(true),
     tags: z.array(z.string()),
     title: z.string(),
     updated: date.optional(),
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import {getCollection} from 'astro:content'
+import * as R from 'rambdax'
 
 export function isoDate(d: Date) {
   return d.toISOString()
@@ -25,16 +26,41 @@ export function shortDate(d: Date) {
   } ${d.getUTCDate()}, ${d.getUTCFullYear()}`
 }
 
+function dateSlug(date: Date) {
+  const yyyy = date.getUTCFullYear()
+  const m = date.getUTCMonth() + 1
+  const mm = m < 10 ? `0${m}` : m
+  const d = date.getUTCDate()
+  const dd = d < 10 ? `0${d}` : d
+  return `${yyyy}-${mm}-${dd}`
+}
+
 export async function getPosts(includeDrafts: boolean = false) {
-  const posts = await getCollection('posts', ({data}) => includeDrafts || !data.draft)
-  return posts.map(post => {
-    const [created] = post.slug.match(/^\d{4}-\d{2}-[^-]+/) || []
-    return {
-      ...post,
-      data: {
-        ...post.data,
-        created: created ? new Date(created) : undefined,
-      },
-    }
-  })
+  const posts = await getCollection(
+    'posts',
+    ({data}) => includeDrafts || !data.draft,
+  )
+  return R.piped(
+    posts,
+    R.map(post => {
+      // internal created can override filename.
+      const createdSlug = post.data.created
+        ? dateSlug(post.data.created)
+        : post.id.match(/^\d{4}-\d{2}-[^-]+/)![0]
+      const slugRest = post.slug.startsWith(`${createdSlug}-`)
+        ? post.slug.substring(11)
+        : post.slug
+      return {
+        ...post,
+        slug: `${createdSlug}-${slugRest}`,
+        data: {
+          created: new Date(createdSlug),
+          ...post.data,
+        },
+      }
+    }),
+    R.sortBy(a => a.data.title.toLowerCase()),
+    R.sortBy(a => a.data.created),
+    R.sortBy(a => Number(a.data.draft)),
+  )
 }
